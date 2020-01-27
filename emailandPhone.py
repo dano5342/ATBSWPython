c0207786bbd79f1b62e22e76bae890d92b4cf979
dano5342 <ellis.daniel0@gmail.com>
2020-01-27 14:44:04 +0100

    Completed a program that uses regex to take all numbers and emails from a document that contains lists of contacts. And can sort through so that just the emails and numbers are displayed, copying them back to the clipboard and pasting them somewhere.

--- emailandPhone.py
+++ emailandPhone.py
@@ -1,11 +1,43 @@
 #! /usr/bin/env python3
+import re, pyperclip
+# A program that scans a document for emails and phone numbers, then compiles a list.
 
-#To Do:Create a regex for phone numbers
+phoneRegex = re.compile(r'''
+(
+(\d{3}|\(\d{3}\))?      # area code 3 first digits
+(\s|-|\.)?          # separators: a hyphen
+(\d{3})         # first three digits of the number
+(\s|-|\.)           # separator A hyphen
+(\d{4})         # last four digits of the number
+(\s*(ext|x|ext.)\s*(\d{2,5}))? # Extension number?
+) 
+''', re.VERBOSE)
 
 #create a regex for email
+emailRegex = re.compile(r'''(
+[a-zA-Z0-9._%+-]+   # username
+@                   # At symbol
+[a-zA-Z0-9.-]+      # Domain name
+(\.[a-zA-Z]{2,4})   # .dotsomething
+)''', re.VERBOSE)
 
-#get the text off the clipboard
+#Find matches in clipboard text.
+text = str(pyperclip.paste())
 
-#extract the email and phone data from clipboard
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
 
-#Copy this extrated data TO the clipboard.
+# Copy results to the clipboard
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No Phone numbers or Email addresses found.')
